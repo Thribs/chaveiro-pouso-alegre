e9a478896424a3326747af38278056fbd1fc5908
Thribs <thribsilva@gmail.com>
2020-12-19 00:38:20 -0300

    working on the services grid

--- pages/servicos.js
+++ pages/servicos.js
@@ -15,48 +15,50 @@ function Servicos(){
             </Link>
         </aside>
         <main>
-           <div className="content">
-               <h2>Serviços</h2>
+            <div className="content">
+                <h2>Serviços</h2>
+                <div className="services-grid">
+                    <div className="service-tile">
+                        <img src="./images/lockpicking.jfif" alt="Pessoa abrindo fechadura"/>
+                        <p>Aberturas de portas residenciais e automotivas</p>
+                    </div>
 
-               <div>
-                   <img src="./images/lockpicking.jfif" alt="Pessoa abrindo fechadura"/>
-                   <h6>Aberturas de portas residenciais e automotivas</h6>
-               </div>
+                    <div className="service-tile">
+                        <img src="./images/cloning key.jfif" alt="Duas chaves numa máquina de cópia"/>
+                        <p>Confecção de chaves codificadas de veículos nacionais e importados</p>
+                    </div>
 
-               <div>
-                   <img src="./images/cloning key.jfif" alt="Duas chaves numa máquina de cópia"/>
-                   <h6>Confecção de chaves codificadas de veículos nacionais e importados</h6>
-               </div>
+                    <div className="service-tile">
+                        <img src="./images/programming key.jfif" alt="Pessoa gravando chave usando equipamento"/>
+                        <p>Reparos e codificação de Chaves para carros, motos e caminhões</p>
+                    </div>
 
-               <div>
-                   <img src="./images/programming key.jfif" alt="Pessoa gravando chave usando equipamento"/>
-                   <h6>Reparos e codificação de Chaves para carros, motos e caminhões</h6>
-               </div>
-               
-               <div>
-                   <img src="./images/opening safe.jfif" alt="Pessoa abrindo cofre"/>
-                   <h6>Abertura de cofres com segredos giratórios e digitais</h6>
-               </div>
+                    <div className="service-tile">
+                        <img src="./images/opening safe.jfif" alt="Pessoa abrindo cofre"/>
+                        <p>Abertura de cofres com segredos giratórios e digitais</p>
+                    </div>
 
-               <div>
-                   <img src="./images/installing lock.jfif" alt="Pessoa instalando fechadura"/>
-                   <h6>Instalação de Fechaduras</h6>
-               </div>
+                    <div className="service-tile">
+                        <img src="./images/installing lock.jfif" alt="Pessoa instalando fechadura"/>
+                        <p>Instalação de Fechaduras</p>
+                    </div>
 
-               <div>
-                   <img src="./images/cloning key2.jfif" alt="Duas chaves numa máquina de cópia"/>
-                   <h6>Cópias de Chaves residenciais e automotivas</h6>
-               </div>
+                    <div className="service-tile">
+                        <img src="./images/cloning key2.jfif" alt="Duas chaves numa máquina de cópia"/>
+                        <p>Cópias de Chaves residenciais e automotivas</p>
+                    </div>
 
-               <div>
-                   <img src="./images/peephole.jfif" alt="Porta com olho mágico"/>
-                   <h6>Instalação de olho mágico</h6>
-               </div>
+                    <div className="service-tile">
+                        <img src="./images/peephole.jfif" alt="Porta com olho mágico"/>
+                        <p>Instalação de olho mágico</p>
+                    </div>
 
-               <div>
-                   <img src="./images/switchblade key.jfif" alt="Chave canivete na mão de uma pessoa"/>
-                   <h6>Chave Canivete codificada, com alarme e telecomando</h6>
-               </div>
+                    <div className="service-tile">
+                        <img src="./images/switchblade key.jfif" alt="Chave canivete na mão de uma pessoa"/>
+                        <p>Chave Canivete codificada, com alarme e telecomando</p>
+                    </div>
+
+                </div>
                <img src="./images/logo-chaveiro-pouso-alegre.png" alt="Logo Chaveiro Pouso Alegre"/>
            </div>
         </main>
